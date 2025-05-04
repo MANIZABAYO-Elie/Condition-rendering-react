31a177b7aca184754265023b72ebbd3f825b80fc
Elie MANIZABAYO <manizabayoelie.com>
2025-05-04 09:46:44 +0200

    modify file

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,36 @@
+ const App = ()=>{
+
+ const Item =({ name, isPacked })=> {
+    return (
+      <li className="item">
+        {name} {isPacked && '✅'}
+      </li>
+    );
+  }
+  
+   
+    return (
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item 
+            isPacked={true} 
+            name="Space suit" 
+          />
+          <Item 
+            isPacked={true} 
+            name="Helmet with a golden leaf" 
+          />
+          <Item 
+            isPacked={false} 
+            name="Photo of Tam" 
+          />
+        </ul>
+      </section>
+    );
+  }
+  
+ export default App
 
 
-function App() {
- 
 
-  return (
-    <>
-     
-    </>
-  )
-}
-
-export default App
